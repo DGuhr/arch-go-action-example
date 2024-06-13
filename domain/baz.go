84e55e89e188bfb3b2fb0944880ef97dc1b03e15
Dominik Guhr <dominik.guhr@innoq.com>
2024-06-13 07:08:10 +0200

    ci should succeed-test.

--- domain/baz.go
+++ domain/baz.go
@@ -1,19 +1,5 @@
 package domain
 
-import (
-	"github.com/spf13/cobra"
-	"os"
-)
-
 func Wrong() {
-	var rootCmd = &cobra.Command{
-		Use:   "test",
-		Short: "test",
-		Long:  `testtest`,
-	}
 
-	if err := rootCmd.Execute(); err != nil {
-		rootCmd.PrintErr(err)
-		os.Exit(-1)
-	}
 }
