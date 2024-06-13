5266e92379362c336631165244ea2a6edbf4e61f
Dominik Guhr <dominik.guhr@innoq.com>
2024-06-13 10:14:10 +0200

    test commit to check if comment shows the right way when check fails

--- domain/baz.go
+++ domain/baz.go
@@ -1,5 +1,19 @@
 package domain
 
-func Wrong() {
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
 
+func Wrong() {
+	var rootCmd = &cobra.Command{
+		Use:   "test",
+		Short: "test",
+		Long:  `testtest`,
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		rootCmd.PrintErr(err)
+		os.Exit(-1)
+	}
 }
